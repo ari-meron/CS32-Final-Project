ccad2e3b4a32e0004121d7c1fc7b5bd4f917fce1
ari-meron <ameron1@college.harvard.edu>
2026-04-28 00:02:10 -0400

    Built the Person class

--- helper_classes/person.py
+++ helper_classes/person.py
@@ -1,5 +1,17 @@
 
 class Person():
 
-    def __init__(self, name, pre_college_experience, im_experience):
+    def __init__(self, name, varsity, pre_college_experience, im_experience):
 
+        self.name = name
+        self.pre_college_experience = pre_college_experience
+        self.im_experience = im_experience
+
+    def is_banned(sport):
+        if varsity == sport:
+            return True
+        return False
+
+    def experience_with(sport):
+        if sport in pre_college_experience:
+            return (pre_college_experience[sport]['experience'], pre_college_experience[sport]['rating'])
