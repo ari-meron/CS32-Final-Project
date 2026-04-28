0907a43a613a695ad26e12de8bde2047d3e286b4
ari-meron <ameron1@college.harvard.edu>
2026-04-28 00:08:57 -0400

    fixed small problems

--- helper_classes/person.py
+++ helper_classes/person.py
@@ -4,14 +4,15 @@ class Person():
     def __init__(self, name, varsity, pre_college_experience, im_experience):
 
         self.name = name
+        self.varsity = varsity
         self.pre_college_experience = pre_college_experience
         self.im_experience = im_experience
 
-    def is_banned(sport):
-        if varsity == sport:
+    def is_banned(self, sport):
+        if self.varsity == sport:
             return True
         return False
 
-    def experience_with(sport):
-        if sport in pre_college_experience:
-            return (pre_college_experience[sport]['experience'], pre_college_experience[sport]['rating'])
+    def experience_with(self, sport):
+        if sport in self.pre_college_experience:
+            return (self.pre_college_experience[sport]['experience'], self.pre_college_experience[sport]['rating'])
