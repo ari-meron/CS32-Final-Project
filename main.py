59a4fdb6263e3a8305592ff90c57cb068291a224
ari-meron <ameron1@college.harvard.edu>
2026-05-03 20:36:58 -0400

    Wrote main

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from parser import load_all, compatibility, sports, people
+from helper_classes.sport import Sport
+from helper_classes.person import Person
+from scorer import direct_score, general_score, build_team
 
+def main():
+    load_all()
+    
+    target_sport = input("What sport do you need a team for?: ")
+    starters, subs = build_team(target_sport)
+
+    print(starters)
+    print(subs)
+
+
+if __name__ == '__main__':
+    main()
